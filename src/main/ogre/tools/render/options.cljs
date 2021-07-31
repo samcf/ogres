d05bee15e3d32dfb9b88778d94e9ef9a550884d8
Sam Ferrell <mail@samcf.me>
2021-07-31 04:03:50 -0600

    Split options forms into multimethod.

--- src/main/ogre/tools/render/options.cljs
+++ src/main/ogre/tools/render/options.cljs
@@ -34,49 +34,52 @@
             (this-as img (handler {:data data :filename (.-name file) :url url :img img}))))
          (set! (.-src image) url))))))
 
-(rum/defc options [props & children]
-  (let [{:keys [element]} props]
-    (rum/with-context [{:keys [data dispatch store]} context]
-      [:div {:class (styles)}
-       (case (:element/type element)
-         :workspace
-         [:<>
-          [:section.header
-           [:label "Workspace Settings"]
-           [:button.close {:type "button" :on-click #(dispatch :view/close (:db/id element))} "×"]]
-          [:section
-           [:label
-            [:input
-             {:type "text"
-              :placeholder "Workspace name"
-              :value (or (:element/name element) "")
-              :on-change
-              (fn [event]
-                (let [value (.. event -target -value)]
-                  (dispatch :element/update (:db/id element) :element/name value)))}]]]
+(defmulti form (fn [{:keys [element]}] (:element/type element)))
 
-          (let [boards (query/boards data)]
-            [:section
-             (when (seq boards)
-               [:div
-                [:label "Select an existing map"]
-                [:div
-                 (for [board boards :let [{:keys [db/id map/url map/name]} board]]
-                   [:div {:key id :style {:background-image (str "url(" url ")")}
-                          :on-click #(dispatch :workspace/change-map (:db/id element) id)}
-                    [:div name]])]])
-             [:input
-              {:type "file"
-               :accept "image/*"
-               :multiple true
-               :on-change
-               #(doseq [file (.. % -target -files)]
-                  (load-image
-                   file
-                   (fn [{:keys [data filename url img]}]
-                     (let [id (ds/squuid) w (.-width img) h (.-height img)]
-                       (-> (.-images store) (.put  #js {:id (str id) :data data :created-at (.now js/Date)}))
-                       (dispatch
-                        :map/create
-                        element
-                        {:map/id id :map/name filename :map/url url :map/width w :map/height h})))))}]])] nil)])))
+(defmethod form :workspace [{:keys [context element]}]
+  (let [{:keys [data dispatch store]} context]
+    [:<>
+     [:section.header
+      [:label "Workspace Settings"]
+      [:button.close {:type "button" :on-click #(dispatch :view/close (:db/id element))} "×"]]
+     [:section
+      [:label
+       [:input
+        {:type "text"
+         :placeholder "Workspace name"
+         :value (or (:element/name element) "")
+         :on-change
+         (fn [event]
+           (let [value (.. event -target -value)]
+             (dispatch :element/update (:db/id element) :element/name value)))}]]]
+
+     (let [boards (query/boards data)]
+       [:section
+        (when (seq boards)
+          [:div
+           [:label "Select an existing map"]
+           [:div
+            (for [board boards :let [{:keys [db/id map/url map/name]} board]]
+              [:div {:key id :style {:background-image (str "url(" url ")")}
+                     :on-click #(dispatch :workspace/change-map (:db/id element) id)}
+               [:div name]])]])
+        [:input
+         {:type "file"
+          :accept "image/*"
+          :multiple true
+          :on-change
+          #(doseq [file (.. % -target -files)]
+             (load-image
+              file
+              (fn [{:keys [data filename url img]}]
+                (let [id (ds/squuid) w (.-width img) h (.-height img)]
+                  (-> (.-images store) (.put  #js {:id (str id) :data data :created-at (.now js/Date)}))
+                  (dispatch
+                   :map/create
+                   element
+                   {:map/id id :map/name filename :map/url url :map/width w :map/height h})))))}]])]))
+
+(rum/defc options [{:keys [element]}]
+  (rum/with-context [value context]
+    [:div {:class (styles)}
+     (form {:context value :element element})]))
