c9aced830a1488fea4ced7e8cf9df5ed92efc3d5
Sam Ferrell <mail@samcf.me>
2021-08-09 03:55:54 -0600

    Link to Github and command to reset local storage.

--- src/main/ogre/tools/render/command.cljs
+++ src/main/ogre/tools/render/command.cljs
@@ -23,7 +23,8 @@
     {:background-color "var(--color-primary-a)"}
     [:svg {:fill "black"}]
     [:div {:border-color "black"}]]]
-  [:button+button {:margin-top "4px"}]
+  [:button+button :button+a :a+button
+   {:margin-top "4px"}]
   [:div.grid
    {:border "1px dashed white"
     :height "16px"
@@ -36,25 +37,34 @@
     :border-color "rgba(255, 255, 255, 0.35)"}])
 
 (defn command [props]
-  (let [{:keys [workspace dispatch]} (uix/context context)
+  (let [{:keys [workspace dispatch store]} (uix/context context)
         {:keys [canvas/mode grid/show]} workspace]
     [:div {:class (styles)}
-     [:button {:type "button" :class (css {:selected (= mode :select)}) :on-click #(dispatch :canvas/select-mode)}
+     [:button {:type "button" :title "Select" :class (css {:selected (= mode :select)}) :on-click #(dispatch :canvas/select-mode)}
       [icon {:name :cursor :width 20 :height 20}]]
-     [:button {:type "button" :class (css {:selected (= mode :board)}) :on-click #(dispatch :canvas/toggle-canvas-options)}
+     [:button {:type "button" :title "Canvas Settings" :class (css {:selected (= mode :board)}) :on-click #(dispatch :canvas/toggle-canvas-options)}
       [icon {:name :image :width 18 :height 18}]]
-     [:button {:type "button" :class (css {:selected (= mode :grid)}) :on-click #(dispatch :canvas/toggle-grid-options)}
+     [:button {:type "button" :title "Grid Settings" :class (css {:selected (= mode :grid)}) :on-click #(dispatch :canvas/toggle-grid-options)}
       [icon {:name :grid :width 21 :height 21}]]
-     [:button {:type "button" :class (css {:selected (= mode :ruler)}) :on-click #(dispatch :canvas/toggle-ruler)}
+     [:button {:type "button" :title "Ruler" :class (css {:selected (= mode :ruler)}) :on-click #(dispatch :canvas/toggle-ruler)}
       [icon {:name :ruler :width 20 :height 20}]]
      [:hr]
-     [:button {:type "button" :class (css {:selected show}) :on-click #(dispatch :grid/toggle)}
+     [:button {:type "button" :title "Toggle Grid" :class (css {:selected show}) :on-click #(dispatch :grid/toggle)}
       [:div.grid]]
      [:hr]
-     [:button {:type "button" :on-click #(dispatch :zoom/in)}
+     [:button {:type "button" :title "Zoom In" :on-click #(dispatch :zoom/in)}
       [icon {:name :zoom-in :width 18 :height 18}]]
-     [:button {:type "button" :on-click #(dispatch :zoom/out)}
+     [:button {:type "button" :title "Zoom Out" :on-click #(dispatch :zoom/out)}
       [icon {:name :zoom-out :width 18 :height 18}]]
      [:hr]
-     [:button {:type "button" :on-click #(dispatch nil)}
-      [icon {:name :info :width 20 :height 20}]]]))
+     [:a {:href "https://www.github.com/samcf/ogre.tools" :title "Project home" :target "_blank"}
+      [:button {:type "button" :on-click #(dispatch nil)}
+       [icon {:name :github :width 20 :height 20}]]]
+     [:button
+      {:type "button"
+       :title "Reset Local Storage"
+       :on-click
+       (fn []
+         (.delete store)
+         (.reload (.-location js/window)))}
+      [icon {:name :recycle :width 20 :height 20}]]]))
