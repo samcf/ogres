2ad8ce6826afcbcbc9331e0837d715ad8e57a781
Sam Ferrell <mail@samcf.me>
2021-09-17 09:35:49 -0600

    New publish script.

---
+++ bin/publish.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+src="$PWD"
+dst=$(mktemp -d -t ogre.tools)
+
+# build
+npm run build
+
+# checkout
+git clone --single-branch --branch gh-pages git@github.com:samcf/ogre.tools.git "$dst"
+
+# copy, commit, and push
+cp -r docs/. "$dst"/
+cd "$dst" || exit
+git add --all
+git commit --allow-empty --amend -m "Publish changes."
+git push --force-with-lease origin gh-pages
+
+# return
+cd "$src" || exit
+rm -rf "$dst"
