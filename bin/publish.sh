785251255bf197ff26dc53f3403c0f5ec6da2445
Sam Ferrell <mail@samcf.me>
2021-09-17 09:54:19 -0600

    Rename docs -> web.

--- bin/publish.sh
+++ bin/publish.sh
@@ -9,7 +9,7 @@ npm run build
 git clone --single-branch --branch gh-pages git@github.com:samcf/ogre.tools.git "$dst"
 
 # copy, commit, and push
-cp -r docs/. "$dst"/
+cp -r web/. "$dst"/
 cd "$dst" || exit
 git add --all
 git commit --allow-empty --amend -m "Publish changes."
