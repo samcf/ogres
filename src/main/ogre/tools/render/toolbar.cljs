720c5aa2ebf12ed18d4d5c7be40626274f87c656
Sam Ferrell <mail@samcf.me>
2022-03-01 13:36:35 -0700

    New toolbar buttons, icons.

--- src/main/ogre/tools/render/toolbar.cljs
+++ src/main/ogre/tools/render/toolbar.cljs
@@ -18,10 +18,14 @@
     :mode/cone   "Draw a cone whose length is equal to its width."
     :mode/line   "Draw a line from one point to another."
     :mode/poly   "Draw a polygon by clicking each point, close by clicking the start."
+    :mode/fog    "Reveal or hide parts of the canvas."
     :share/open  [:span "Open or close the player window. "
                   [:a {:href "https://github.com/samcf/ogre.tools/wiki#player-window" :target "_blank"} "Learn more"] "."]
     :share/play  "Resumes updates to the player window."
-    :share/pause "Pauses updates to the player window. Good time to setup an ambush!" ""))
+    :share/pause "Pauses updates to the player window. Good time to setup an ambush!"
+    :fog/hide    "Hide the entire canvas."
+    :fog/show    "Reveal the entire canvas."
+    ""))
 
 (def query
   {:pull
@@ -46,6 +50,11 @@
            :on-click #(dispatch :canvas/toggle-mode mode)
            :on-mouse-enter #(reset! tooltip-key (keyword "mode" (name mode)))})
 
+        tooltip-fn
+        (fn [key]
+          (fn []
+            (reset! tooltip-key key)))
+
         element
         (if (and (not (nil? @tooltip-key))
                  (:root/tooltips? data))
@@ -65,17 +74,17 @@
        [:button.toolbar-zoom
         {:disabled (= (:zoom/scale canvas) 1)
          :on-click #(dispatch :zoom/reset)
-         :on-mouse-enter #(reset! tooltip-key :zoom/reset)}
+         :on-mouse-enter (tooltip-fn :zoom/reset)}
         (-> (:zoom/scale canvas) (* 100) (js/Math.trunc) (str "%"))]
        [:button
         {:disabled (= (:zoom/scale canvas) 0.15)
          :on-click #(dispatch :zoom/out)
-         :on-mouse-enter #(reset! tooltip-key :zoom/out)}
+         :on-mouse-enter (tooltip-fn :zoom/out)}
         [icon {:name "zoom-out"}]]
        [:button
         {:disabled (= (:zoom/scale canvas) 4)
          :on-click #(dispatch :zoom/in)
-         :on-mouse-enter #(reset! tooltip-key :zoom/in)}
+         :on-mouse-enter (tooltip-fn :zoom/in)}
         [icon {:name "zoom-in"}]]]
       [:div.toolbar-group
        [:button (mode-attrs :select) [icon {:name "cursor"}] [shortcut "S"]]
@@ -83,21 +92,29 @@
        [:button (mode-attrs :circle) [icon {:name "circle"}] [shortcut "1"]]
        [:button (mode-attrs :rect) [icon {:name "square"}] [shortcut "2"]]
        [:button (mode-attrs :cone) [icon {:name "triangle"}] [shortcut "3"]]
-       [:button (mode-attrs :poly) [icon {:name "pentagon"}] [shortcut "4"]]
+       [:button (mode-attrs :poly) [icon {:name "star"}] [shortcut "4"]]
        [:button (mode-attrs :line) [icon {:name "slash-lg"}] [shortcut "5"]]]
+      [:div.toolbar-group
+       [:button {:on-mouse-enter (tooltip-fn :fog/hide)}
+        [icon {:name "mask"}]]
+       [:button {:on-mouse-enter (tooltip-fn :fog/show)}
+        [icon {:name "eraser"}]]
+       [:button (mode-attrs :fog)
+        [icon {:name "star-half"}]
+        [shortcut "F"]]]
       [:div.toolbar-group
        [:button
         {:css {:active (:share/open? data)}
          :on-click #(dispatch :share/initiate)
-         :on-mouse-enter #(reset! tooltip-key :share/open)}
+         :on-mouse-enter (tooltip-fn :share/open)}
         [icon {:name "pip" :size 22}] [shortcut "W"]]
        [:button
         {:disabled (or (not (:share/open? data)) (not (:share/paused? data)))
          :on-click #(dispatch :share/switch)
-         :on-mouse-enter #(reset! tooltip-key :share/play)}
+         :on-mouse-enter (tooltip-fn :share/play)}
         [icon {:name "play-fill" :size 22}]]
        [:button
         {:disabled (or (not (:share/open? data)) (:share/paused? data))
          :on-click #(dispatch :share/switch)
-         :on-mouse-enter #(reset! tooltip-key :share/pause)}
+         :on-mouse-enter (tooltip-fn :share/pause)}
         [icon {:name "pause-fill" :size 22}]]]]]))
