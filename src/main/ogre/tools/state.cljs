203ff2424df1c7b3de688f260a360e37efafa420
Sam Ferrell <mail@samcf.me>
2022-05-19 17:24:01 -0600

    Be more explicit about loading state.

--- src/main/ogre/tools/state.cljs
+++ src/main/ogre/tools/state.cljs
@@ -46,6 +46,7 @@
     [:db/add -2 :entity/key (squuid)]
     [:db/add -3 :db/ident :local]
     [:db/add -3 :entity/key (squuid)]
+    [:db/add -3 :local/loaded? false]
     [:db/add -3 :local/window -4]
     [:db/add -3 :local/windows -4]
     [:db/add -3 :local/type (local-type)]
