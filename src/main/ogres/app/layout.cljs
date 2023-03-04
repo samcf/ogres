b8234af8978bd5a3f9aebe3442bedf4b526e68d2
Sam Ferrell <mail@samcf.me>
2023-03-03 18:35:35 -0700

    Re-include necessary modal portal.

--- src/main/ogres/app/layout.cljs
+++ src/main/ogres/app/layout.cljs
@@ -1,5 +1,5 @@
 (ns ogres.app.layout
-  (:require [ogres.app.hooks :refer [use-query]]
+  (:require [ogres.app.hooks :refer [use-query create-portal]]
             [ogres.app.render.canvas :refer [canvas]]
             [ogres.app.render.panel :refer [container]]
             [ogres.app.render.toolbar :refer [toolbar]]
@@ -24,5 +24,8 @@
           (if (= type :host)
             [:div.layout-workspaces [workspaces]])
           [:div.layout-canvas [canvas]]
+          [create-portal
+           (fn [ref]
+             [:div.layout-modal {:ref ref}]) :modal]
           [:div.layout-toolbar [toolbar]]
           [:div.layout-panel [container]]]))]))
