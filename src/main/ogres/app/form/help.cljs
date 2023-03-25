a6aa436fcaa231c8f9b4007d88a2c4729d3a236f
Sam Ferrell <mail@samcf.me>
2023-03-25 10:30:20 -0600

    Updated content and design for the help panel.

--- src/main/ogres/app/form/help.cljs
+++ src/main/ogres/app/form/help.cljs
@@ -1,43 +1,61 @@
 (ns ogres.app.form.help
   (:require [ogres.app.env :refer [VERSION]]
-            [ogres.app.form.render :refer [form]]
-            [ogres.app.hooks :refer [use-dispatch use-query]]
-            [ogres.app.render :refer [checkbox]]))
+            [ogres.app.form.render :as render]
+            [ogres.app.hooks :refer [use-dispatch use-query]]))
 
-(def ^{:private true} query
+(def ^:private resource-links
+  [["https://github.com/samcf/ogres" "GitHub repository page" "Source Code"]
+   ["https://github.com/samcf/ogres/discussions" "Ask a Question" "Ask a Question"]
+   ["https://github.com/samcf/ogres/wiki" "Project wiki" "Wiki"]])
+
+(def ^:private query
   [[:local/shortcuts? :default true]
    [:local/tooltips? :default true]])
 
-(defn ^{:private true} help []
+(defn- form []
   (let [dispatch (use-dispatch)
         result   (use-query query)
         {:local/keys [shortcuts? tooltips?]} result]
-    [:section
-     [:fieldset.setting
-      [:label "Show Shortcuts"]
-      (for [display? [false true] :let [checked? (= display? shortcuts?)]]
-        ^{:key display?}
-        [checkbox
-         {:checked checked?
-          :on-change
-          (fn []
-            (if (not checked?)
-              (dispatch :interface/toggle-shortcuts display?)))}
-         (if display? "Yes" "No")])]
-     [:fieldset.setting
-      [:label "Show Tooltips"]
-      (for [display? [false true] :let [checked? (= display? tooltips?)]]
-        ^{:key display?}
-        [checkbox
-         {:checked checked?
-          :on-change
-          (fn []
-            (if (not checked?)
-              (dispatch :interface/toggle-tooltips display?)))}
-         (if display? "Yes" "No")])]
-     [:button.ogre-button {:on-click #(dispatch :storage/reset) :style {:margin-top 8}}
-      "Restore Factory Defaults"]
-     [:span "Current version: " [:strong VERSION]]]))
+    [:section.help
+     [:section
+      [:header "Interface preferences"]
+      [:fieldset
+       [:input
+        {:id        "show-shortcuts"
+         :type      "checkbox"
+         :checked   shortcuts?
+         :on-change (fn [event]
+                      (let [checked (.. event -target -checked)]
+                        (dispatch :interface/toggle-shortcuts checked)))}]
+       [:label {:for "show-shortcuts"} "Show shortcuts"]]
+      [:fieldset
+       [:input
+        {:id        "show-tooltips"
+         :type      "checkbox"
+         :checked   tooltips?
+         :on-change (fn [event]
+                      (let [checked (.. event -target -checked)]
+                        (dispatch :interface/toggle-tooltips checked)))}]
+       [:label {:for "show-tooltips"} "Show tooltips"]]]
+     [:section
+      [:header "Data"]
+      [:p "All application data is saved locally on your browser, including
+           any images you upload for maps or tokens. Any changes you make
+           are automatically saved. As long as you use the same browser and
+           don't clear your browser's cache, all your changes will stick
+           around even if you close the browser or restart your computer."]
+      [:br]
+      [:button.button {:on-click #(dispatch :storage/reset) :style {:margin-bottom 8}}
+       "Delete Local Data"]
+      [:p "Click this button to delete all application data, restoring this
+           application to its original empty state. All your work and images
+           will be removed."]
+      [:br]
+      [:p "Current version: " [:code VERSION]]]
+     [:section
+      [:header "Resources"]
+      [:ul
+       (for [[url title label] resource-links]
+         [:li [:a {:href url :title title :target "_blank"} label]])]]]))
 
-(defmethod form :help []
-  help)
+(defmethod render/form :help [] form)
