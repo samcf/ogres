c9057f3326b231a5ad3d711142fcd8ea6ae82fd2
Sam Ferrell <mail@samcf.me>
2023-05-11 15:32:01 -0600

    Resolve error boundary uix usage.

--- src/main/ogres/app/render.cljs
+++ src/main/ogres/app/render.cljs
@@ -36,20 +36,20 @@
 
 (def error-boundary
   (create-error-boundary
-   {:error->state (fn [error] {:error error})
-    :handle-catch (fn [])}
-   (fn [[{:keys [error]}] {:keys [children]}]
+   {:derive-error-state (fn [error] {:error error})}
+   (fn [[state _] {:keys [children]}]
      (let [store (initialize)]
-       (if error
-         [:div {:style {:padding "32px" :max-width 640}}
-          [:p "The application crashed! Try refreshing the page. If the problem
+       (if (:error state)
+         ($ :div {:style {:padding "32px" :max-width 640}}
+           ($ :p "The application crashed! Try refreshing the page. If the problem
                persists, click the button below to delete your local data. This
-               may resolve your issue."]
-          [:button
-           {:on-click
-            (fn []
-              (.delete store)
-              (.reload (.-location js/window)))} "Delete your local data"]]
+               may resolve your issue.")
+           ($ :br)
+           ($ :button.button.button-danger
+             {:on-click
+              (fn []
+                (.delete store)
+                (.reload (.-location js/window)))} "Delete your local data"))
          children)))))
 
 (defui pagination
