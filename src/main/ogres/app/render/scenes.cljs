03da72f21e66b521650c76ec8eed9729060a7406
Sam Ferrell <mail@samcf.me>
2023-07-28 17:55:55 -0600

    Improved confirmation prompt when closing scenes.

--- src/main/ogres/app/render/scenes.cljs
+++ src/main/ogres/app/render/scenes.cljs
@@ -4,6 +4,13 @@
             [ogres.app.render :refer [css]]
             [uix.core :refer [defui $]]))
 
+(defn ^{:private true} close-prompt [label]
+  (str "Are you sure you want to close "
+       (if (blank? label)
+         (str "this scene")
+         (str "'" label "'"))
+       "? This action cannot be undone."))
+
 (def query
   [{:local/camera [:db/key]}
    {:local/cameras [:db/key :camera/label]}])
@@ -19,9 +26,10 @@
           ($ :div {:on-click #(dispatch :scenes/change key)}
             (if (blank? label) ($ :em "New scene") (trim label)))
           ($ :button
-            {:type "button"
-             :on-click #(when (js/confirm (str "Close " (when (not (blank? label)) (str label " ")) "scene?"))
-                          (dispatch :scenes/remove key))
-             :title "Close scene"}
+            {:type "button" :title "Close this scene."
+             :on-click
+             (fn []
+               (if (js/confirm (close-prompt label))
+                 (dispatch :scenes/remove key)))}
             "×")))
       ($ :button {:type "button" :on-click #(dispatch :scenes/create) :title "Create new scene"} "+"))))
