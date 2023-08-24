56b5d615a0ee62f5a1bcc1d4e63e4bdef5f726db
Sam Ferrell <mail@samcf.me>
2023-08-23 18:42:51 -0600

    Improve look and feel of scene tabs.

--- src/main/ogres/app/render/scenes.cljs
+++ src/main/ogres/app/render/scenes.cljs
@@ -1,7 +1,7 @@
 (ns ogres.app.render.scenes
   (:require [clojure.string :refer [blank? trim]]
             [ogres.app.hooks :refer [use-dispatch use-query]]
-            [ogres.app.render :refer [css]]
+            [ogres.app.render :refer [css icon]]
             [uix.core :refer [defui $]]))
 
 (defn ^:private close-prompt
@@ -25,12 +25,12 @@
       (for [{:keys [db/key camera/label]} cameras]
         ($ :div {:key key :class (css {:selected (= key (:db/key current))})}
           ($ :div {:on-click #(dispatch :scenes/change key)}
-            (if (blank? label) ($ :em "New scene") (trim label)))
+            (if (blank? label) "New scene" (trim label)))
           ($ :button
-            {:type "button" :title "Close this scene."
+            {:type "button" :title "Remove scene"
              :on-click
              (fn []
                (if (js/confirm (close-prompt label))
                  (dispatch :scenes/remove key)))}
-            "×")))
+            ($ icon {:name "x-circle-fill" :size 16}))))
       ($ :button {:type "button" :on-click #(dispatch :scenes/create) :title "Create new scene"} "+"))))
