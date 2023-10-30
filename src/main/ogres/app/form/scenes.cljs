d31a2ff6d58db8ec9f614fdc8df57106ececeb22
Sam Ferrell <mail@samcf.me>
2023-10-30 14:40:57 -0600

    Preview images by displaying it in modal.

--- src/main/ogres/app/form/scenes.cljs
+++ src/main/ogres/app/form/scenes.cljs
@@ -2,7 +2,8 @@
   (:require [clojure.string :refer [blank?]]
             [ogres.app.hooks :refer [use-dispatch use-image use-image-uploader use-query]]
             [ogres.app.render :refer [icon pagination]]
-            [uix.core :as uix :refer [defui $ use-ref use-state]]))
+            [uix.core :as uix :refer [defui $ use-ref use-state]]
+            [uix.dom :refer [create-portal]]))
 
 (def ^:private options-vis
   [["Revealed" :revealed] ["Obscured" :dimmed] ["Hidden" :hidden]])
@@ -44,7 +45,8 @@
     (children {:data-url data-url})))
 
 (defui form []
-  (let [dispatch (use-dispatch)
+  (let [[preview set-preview] (use-state nil)
+        dispatch (use-dispatch)
         upload   (use-image-uploader {:type :scene})
         input    (use-ref)
         result   (use-query query [:db/ident :root])
@@ -54,80 +56,79 @@
           camera :local/camera} :root/local} result
         [page set-page] (use-state 1)]
     ($ :<>
-      ($ :section
-        ($ :div.scene-options
-          ($ :fieldset.text {:style {:grid-area "title"}}
+      ($ :section.scene-options
+        ($ :fieldset.text {:style {:grid-area "title"}}
+          ($ :input
+            {:type "text"
+             :title "Scene name"
+             :maxLength 36
+             :spellCheck false
+             :placeholder "Name"
+             :value (:camera/label camera)
+             :on-change
+             (fn [event]
+               (let [value (.. event -target -value)]
+                 (dispatch :camera/change-label value)))}))
+        ($ :fieldset.text {:style {:grid-area "tile-size"}}
+          ($ :input
+            {:type "number"
+             :title "Tile size (px)"
+             :value (or (:scene/grid-size scene) "")
+             :disabled (nil? (:scene/image scene))
+             :placeholder "Tile size (px)"
+             :on-change
+             (fn [event]
+               (let [value (.. event -target -value)
+                     value (js/Number value)]
+                 (if (= value 0)
+                   (dispatch :scene/retract-grid-size)
+                   (dispatch :scene/change-grid-size value))))}))
+        ($ :fieldset.option {:style {:grid-area "show-grid"}}
+          ($ :input
+            {:id "show-grid"
+             :type "checkbox"
+             :checked (:scene/show-grid scene)
+             :on-change #(dispatch :scene/toggle-show-grid (.. % -target -checked))})
+          ($ :label {:for "show-grid"} "Show grid"))
+        ($ :fieldset.option {:style {:grid-area "dark-grid"}}
+          ($ :input
+            {:id "dark-grid"
+             :type "checkbox"
+             :checked (:scene/dark-mode scene)
+             :on-change #(dispatch :scene/toggle-dark-mode (.. % -target -checked))})
+          ($ :label {:for "dark-grid"} "Use dark grid"))
+        ($ :fieldset.option {:style {:grid-area "grid-align"}}
+          ($ :input
+            {:id "grid-align"
+             :type "checkbox"
+             :checked false
+             :disabled true})
+          ($ :label {:for "grid-align"} "Align to grid"))
+        (for [[label value] options-vis
+              :let [on-change #(dispatch :scene/change-lighting value)]]
+          ($ :fieldset.option {:key value :style {:grid-area (str "visi-" (name value))}}
             ($ :input
-              {:type "text"
-               :title "Scene name"
-               :maxLength 36
-               :spellCheck false
-               :placeholder "Name"
-               :value (:camera/label camera)
-               :on-change
-               (fn [event]
-                 (let [value (.. event -target -value)]
-                   (dispatch :camera/change-label value)))}))
-          ($ :fieldset.text {:style {:grid-area "tile-size"}}
-            ($ :input
-              {:type "number"
-               :title "Tile size (px)"
-               :value (or (:scene/grid-size scene) "")
+              {:id (name value)
+               :type "radio"
+               :name "visi"
+               :value value
+               :checked (= (:scene/lighting scene) value)
                :disabled (nil? (:scene/image scene))
-               :placeholder "Tile size (px)"
-               :on-change
-               (fn [event]
-                 (let [value (.. event -target -value)
-                       value (js/Number value)]
-                   (if (= value 0)
-                     (dispatch :scene/retract-grid-size)
-                     (dispatch :scene/change-grid-size value))))}))
-          ($ :fieldset.option {:style {:grid-area "show-grid"}}
+               :on-change on-change})
+            ($ :label {:for (name value)} label)))
+        (for [[label value] options-day
+              :let [on-change #(dispatch :scene/change-time-of-day value)]]
+          ($ :fieldset.option {:key value :style {:grid-area (str "tofd-" (name value))}}
             ($ :input
-              {:id "show-grid"
-               :type "checkbox"
-               :checked (:scene/show-grid scene)
-               :on-change #(dispatch :scene/toggle-show-grid (.. % -target -checked))})
-            ($ :label {:for "show-grid"} "Show grid"))
-          ($ :fieldset.option {:style {:grid-area "dark-grid"}}
-            ($ :input
-              {:id "dark-grid"
-               :type "checkbox"
-               :checked (:scene/dark-mode scene)
-               :on-change #(dispatch :scene/toggle-dark-mode (.. % -target -checked))})
-            ($ :label {:for "dark-grid"} "Use dark grid"))
-          ($ :fieldset.option {:style {:grid-area "grid-align"}}
-            ($ :input
-              {:id "grid-align"
-               :type "checkbox"
-               :checked false
-               :disabled true})
-            ($ :label {:for "grid-align"} "Align to grid"))
-          (for [[label value] options-vis
-                :let [on-change #(dispatch :scene/change-lighting value)]]
-            ($ :fieldset.option {:key value :style {:grid-area (str "visi-" (name value))}}
-              ($ :input
-                {:id (name value)
-                 :type "radio"
-                 :name "visi"
-                 :value value
-                 :checked (= (:scene/lighting scene) value)
-                 :disabled (nil? (:scene/image scene))
-                 :on-change on-change})
-              ($ :label {:for (name value)} label)))
-          (for [[label value] options-day
-                :let [on-change #(dispatch :scene/change-time-of-day value)]]
-            ($ :fieldset.option {:key value :style {:grid-area (str "tofd-" (name value))}}
-              ($ :input
-                {:id (name value)
-                 :type "radio"
-                 :name "tofd"
-                 :value value
-                 :checked (= (:scene/timeofday scene) value)
-                 :disabled (nil? (:scene/image scene))
-                 :on-change on-change})
-              ($ :label {:for (name value)} label)))))
-      ($ :section
+              {:id (name value)
+               :type "radio"
+               :name "tofd"
+               :value value
+               :checked (= (:scene/timeofday scene) value)
+               :disabled (nil? (:scene/image scene))
+               :on-change on-change})
+            ($ :label {:for (name value)} label))))
+      ($ :section.scene-images
         ($ :fieldset.scene-gallery
           (let [src (* (max (dec (min page pages)) 0) per-page)
                 dst (min (+ src per-page) (count data))
@@ -149,6 +150,13 @@
                        (fn [event]
                          (.stopPropagation event)
                          (dispatch :scene/change-image checksum))}
+                      ($ :button.button.button-neutral
+                        {:type "button"
+                         :on-click
+                         (fn [event]
+                           (.stopPropagation event)
+                           (set-preview checksum))}
+                        ($ icon {:name "zoom-in" :size 18}))
                       ($ :button.button.button-danger
                         {:type "button"
                          :on-click
@@ -177,12 +185,33 @@
             ($ pagination
               {:pages pages
                :value (min page pages)
-               :on-change set-page})
-            ($ :nav))))
-      ($ :section
+               :on-change set-page})))
+        (if (not (nil? preview))
+          (if-let [node (js/document.querySelector "#root")]
+            (create-portal
+             ($ :.scene-gallery-modal
+               ($ :.scene-gallery-modal-container
+                 ($ thumbnail {:checksum preview}
+                   (fn [{:keys [data-url]}]
+                     ($ :figure.scene-gallery-modal-preview
+                       {:style {:background-image (str "url(" data-url ")")}})))
+                 ($ :.scene-gallery-modal-footer
+                   ($ :button.button.button-danger
+                     {:style {:margin-right "auto"}
+                      :on-click (fn [] (set-preview nil) (dispatch :scenes/remove preview))}
+                     ($ icon {:name "trash3-fill" :size 16}))
+                   ($ :button.button.button-neutral
+                     {:on-click #(set-preview nil)}
+                     "Close")
+                   ($ :button.button.button-primary
+                     {:style {:min-width 120 :justify-content "center"}
+                      :on-click (fn [] (set-preview nil) (dispatch :scene/change-image preview))}
+                     "Apply"))))
+             node))))
+      ($ :section.scene-scenes
         ($ :header "Scenes")
         ($ :ul.scene-list
-          (for [entity (:local/cameras (:root/local result))
+          (for [entity (get-in result [:root/local :local/cameras])
                 :let [on-select #(dispatch :scenes/change (:db/key entity))
                       on-remove #(dispatch :scenes/remove (:db/key entity))]]
             ($ :li.scene-list-item
