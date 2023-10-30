3bd6645bae19147b3f2c442ddce7fd4ba428a20f
Sam Ferrell <mail@samcf.me>
2023-10-30 17:09:58 -0600

    Display image, scene counts. Minor refactoring, renaming.

--- src/main/ogres/app/form/scenes.cljs
+++ src/main/ogres/app/form/scenes.cljs
@@ -47,13 +47,11 @@
 (defui form []
   (let [[preview set-preview] (use-state nil)
         dispatch (use-dispatch)
-        upload   (use-image-uploader {:type :scene})
+        uploader (use-image-uploader {:type :scene})
         input    (use-ref)
-        result   (use-query query [:db/ident :root])
-        data     (vec (:root/scene-images result))
-        pages    (int (js/Math.ceil (/ (count data) per-page)))
+        data     (use-query query [:db/ident :root])
         {{{scene :camera/scene} :local/camera
-          camera :local/camera} :root/local} result
+          camera :local/camera} :root/local} data
         [page set-page] (use-state 1)]
     ($ :<>
       ($ :section.scene-options
@@ -128,18 +126,18 @@
                :disabled (nil? (:scene/image scene))
                :on-change on-change})
             ($ :label {:for (name value)} label))))
-      ($ :section.scene-images
-        ($ :.scene-images-label "Background images")
-        ($ :fieldset.scene-gallery
-          (let [src (* (max (dec (min page pages)) 0) per-page)
-                dst (min (+ src per-page) (count data))
-                seq (->> (repeat per-page :placeholder)
-                         (into (subvec data src dst))
-                         (take per-page)
-                         (map-indexed vector))]
-            (for [[idx data] seq
-                  :let [selected (= (:image/checksum data)
-                                    (:image/checksum (:scene/image scene)))]]
+      (let [img (vec (:root/scene-images data))
+            pgs (int (js/Math.ceil (/ (count img) per-page)))
+            src (* (max (dec (min page pgs)) 0) per-page)
+            dst (min (+ src per-page) (count img))
+            pag (->> (repeat per-page :placeholder)
+                     (into (subvec img src dst))
+                     (take per-page)
+                     (map-indexed vector))]
+        ($ :section.scene-images
+          ($ :.scene-images-label "Background images" " " "[" (count img) "]")
+          ($ :fieldset.scene-gallery
+            (for [[idx data] pag :let [selected (= (:image/checksum data) (:image/checksum (:scene/image scene)))]]
               (if-let [checksum (:image/checksum data)]
                 ($ thumbnail {:key idx :checksum checksum}
                   (fn [{:keys [data-url]}]
@@ -166,78 +164,79 @@
                            (dispatch :scene-images/remove checksum))}
                         ($ icon {:name "trash3-fill" :size 18})))))
                 ($ :figure.scene-gallery-thumbnail
-                  {:key idx :data-type "placeholder"})))))
-        ($ :fieldset.scene-gallery-form
-          ($ :button.button.button-neutral
-            {:on-click #(.click (deref input))}
-            ($ :input
-              {:ref input
-               :type "file"
-               :hidden true
-               :accept "image/*"
-               :multiple true
-               :on-change
-               (fn [event]
-                 (doseq [file (.. event -target -files)]
-                   (upload file))
-                 (set! (.. event -target -value) ""))})
-            ($ icon {:name "camera-fill" :size 16}) "Upload images")
-          (if (> pages 1)
-            ($ pagination
-              {:pages pages
-               :value (min page pages)
-               :on-change set-page})))
-        (if (not (nil? preview))
-          (if-let [node (js/document.querySelector "#root")]
-            (create-portal
-             ($ :.scene-gallery-modal
-               ($ :.scene-gallery-modal-container
-                 ($ thumbnail {:checksum preview}
-                   (fn [{:keys [data-url]}]
-                     ($ :figure.scene-gallery-modal-preview
-                       {:style {:background-image (str "url(" data-url ")")}})))
-                 ($ :.scene-gallery-modal-footer
-                   ($ :button.button.button-danger
-                     {:style {:margin-right "auto"}
-                      :on-click (fn [] (set-preview nil) (dispatch :scene-images/remove preview))}
-                     ($ icon {:name "trash3-fill" :size 16}))
-                   ($ :button.button.button-neutral
-                     {:on-click #(set-preview nil)}
-                     "Close")
-                   ($ :button.button.button-primary
-                     {:on-click (fn [] (set-preview nil) (dispatch :scene/change-image preview))}
-                     "Apply background"))))
-             node))))
-      ($ :section.scene-scenes
-        ($ :header "Scenes")
-        ($ :ul.scene-list
-          (for [entity (get-in result [:root/local :local/cameras])
-                :let [on-select #(dispatch :scenes/change (:db/key entity))
-                      on-remove #(dispatch :scenes/remove (:db/key entity))]]
+                  {:key idx :data-type "placeholder"}))))
+          ($ :fieldset.scene-gallery-form
+            ($ :button.button.button-neutral
+              {:on-click #(.click (deref input))}
+              ($ :input
+                {:ref input
+                 :type "file"
+                 :hidden true
+                 :accept "image/*"
+                 :multiple true
+                 :on-change
+                 (fn [event]
+                   (doseq [file (.. event -target -files)]
+                     (uploader file))
+                   (set! (.. event -target -value) ""))})
+              ($ icon {:name "camera-fill" :size 16}) "Upload images")
+            (if (> pgs 1)
+              ($ pagination
+                {:pages pgs
+                 :value (min pag pgs)
+                 :on-change set-page})))
+          (if (not (nil? preview))
+            (if-let [node (js/document.querySelector "#root")]
+              (create-portal
+               ($ :.scene-gallery-modal
+                 ($ :.scene-gallery-modal-container
+                   ($ thumbnail {:checksum preview}
+                     (fn [{:keys [data-url]}]
+                       ($ :figure.scene-gallery-modal-preview
+                         {:style {:background-image (str "url(" data-url ")")}})))
+                   ($ :.scene-gallery-modal-footer
+                     ($ :button.button.button-danger
+                       {:style {:margin-right "auto"}
+                        :on-click (fn [] (set-preview nil) (dispatch :scene-images/remove preview))}
+                       ($ icon {:name "trash3-fill" :size 16}))
+                     ($ :button.button.button-neutral
+                       {:on-click #(set-preview nil)}
+                       "Close")
+                     ($ :button.button.button-primary
+                       {:on-click (fn [] (set-preview nil) (dispatch :scene/change-image preview))}
+                       "Change background"))))
+               node)))))
+      (let [cameras (get-in data [:root/local :local/cameras])]
+        ($ :section.scene-scenes
+          ($ :header "Scenes" " " "[" (count cameras) "]")
+          ($ :ul.scene-list
+            (for [entity cameras
+                  :let [on-select #(dispatch :scenes/change (:db/key entity))
+                        on-remove #(dispatch :scenes/remove (:db/key entity))]]
+              ($ :li.scene-list-item
+                {:key (:db/key entity) :data-selected (= (:db/key entity) (:db/key camera))}
+                (if-let [checksum (:image/checksum (:scene/image (:camera/scene entity)))]
+                  ($ thumbnail {:checksum checksum}
+                    (fn [{:keys [data-url]}]
+                      ($ :.scene-list-item-image
+                        {:style {:background-image (str "url(" data-url ")")}
+                         :on-click on-select
+                         :data-image "image"})))
+                  ($ :.scene-list-item-image
+                    {:on-click on-select :data-image "empty"}))
+                (if-let [label (:camera/label entity)]
+                  ($ :.scene-list-item-label label)
+                  ($ :.scene-list-item-label "New scene"))
+                ($ :button.scene-list-item-remove.button.button-neutral
+                  {:on-click
+                   (fn []
+                     (if (js/confirm (remove-prompt (:camera/label entity)))
+                       (on-remove)))}
+                  ($ icon {:name "trash3-fill" :size 16}))))
             ($ :li.scene-list-item
-              {:key (:db/key entity) :data-selected (= (:db/key entity) (:db/key camera))}
-              (if-let [checksum (:image/checksum (:scene/image (:camera/scene entity)))]
-                ($ thumbnail {:checksum checksum}
-                  (fn [{:keys [data-url]}]
-                    ($ :.scene-list-item-image
-                      {:style {:background-image (str "url(" data-url ")")}
-                       :on-click on-select
-                       :data-image "image"})))
-                ($ :.scene-list-item-image
-                  {:on-click on-select :data-image "empty"}))
-              (if-let [label (:camera/label entity)]
-                ($ :.scene-list-item-label label)
-                ($ :.scene-list-item-label "New scene"))
+              {:data-placeholder true}
+              ($ :.scene-list-item-image {:data-image "placeholder"})
+              ($ :.scene-list-item-label)
               ($ :button.scene-list-item-remove.button.button-neutral
-                {:on-click
-                 (fn []
-                   (if (js/confirm (remove-prompt (:camera/label entity)))
-                     (on-remove)))}
-                ($ icon {:name "trash3-fill" :size 16}))))
-          ($ :li.scene-list-item
-            {:data-placeholder true}
-            ($ :.scene-list-item-image {:data-image "placeholder"})
-            ($ :.scene-list-item-label)
-            ($ :button.scene-list-item-remove.button.button-neutral
-              {:on-click #(dispatch :scenes/create)}
-              ($ icon {:name "plus" :size 24}))))))))
+                {:on-click #(dispatch :scenes/create)}
+                ($ icon {:name "plus" :size 24})))))))))
