35f42c15634c64412d79acc06a0fe5b3e3010da3
Sam Ferrell <mail@samcf.me>
2023-12-29 10:18:18 -0700

    Only display version section for the host.

--- src/main/ogres/app/form/help.cljs
+++ src/main/ogres/app/form/help.cljs
@@ -1,6 +1,6 @@
 (ns ogres.app.form.help
   (:require [ogres.app.const :refer [VERSION]]
-            [ogres.app.hooks :refer [use-dispatch]]
+            [ogres.app.hooks :refer [use-dispatch use-query]]
             [ogres.app.shortcut :refer [shortcuts]]
             [ogres.app.provider.release :as release]
             [uix.core :refer [defui $ use-context]]))
@@ -19,34 +19,36 @@
 
 (defui form []
   (let [releases (use-context release/context)
-        dispatch (use-dispatch)]
+        dispatch (use-dispatch)
+        result   (use-query [[:local/type :default :conn]] [:db/ident :local])]
     ($ :section.help
-      ($ :section
-        ($ :header "Version" " [ " VERSION " ]")
-        ($ :div.form-notice
-          (if-let [latest (last releases)]
-            (if (not= VERSION latest)
-              ($ :<>
-                ($ :p ($ :strong "There are updates available!"))
-                ($ :p "Upgrading to the latest version will "
-                  ($ :strong "delete all your local data") ". "
-                  "Only upgrade if you are ready to start over from scratch.")
-                ($ :br)
-                ($ :button.button.button-primary
-                  {:on-click
-                   (fn []
-                     (if-let [_ (js/confirm confirm-upgrade)]
-                       (dispatch :storage/reset)))} "Upgrade to latest version [ " latest " ]"))
-              ($ :<>
-                ($ :p ($ :strong "You're on the latest version."))
-                ($ :p "Pressing this button will delete all your local data and
+      (if (= (:local/type result) :host)
+        ($ :section
+          ($ :header "Version" " [ " VERSION " ]")
+          ($ :div.form-notice
+            (if-let [latest (last releases)]
+              (if (not= VERSION latest)
+                ($ :<>
+                  ($ :p ($ :strong "There are updates available!"))
+                  ($ :p "Upgrading to the latest version will "
+                    ($ :strong "delete all your local data") ". "
+                    "Only upgrade if you are ready to start over from scratch.")
+                  ($ :br)
+                  ($ :button.button.button-primary
+                    {:on-click
+                     (fn []
+                       (if-let [_ (js/confirm confirm-upgrade)]
+                         (dispatch :storage/reset)))} "Upgrade to latest version [ " latest " ]"))
+                ($ :<>
+                  ($ :p ($ :strong "You're on the latest version."))
+                  ($ :p "Pressing this button will delete all your local data and
                        restore the application to its original state.")
-                ($ :br)
-                ($ :button.button.button-neutral
-                  {:on-click
-                   (fn []
-                     (if-let [_ (js/confirm confirm-delete)]
-                       (dispatch :storage/reset)))} "Delete local data"))))))
+                  ($ :br)
+                  ($ :button.button.button-neutral
+                    {:on-click
+                     (fn []
+                       (if-let [_ (js/confirm confirm-delete)]
+                         (dispatch :storage/reset)))} "Delete local data")))))))
       ($ :section
         ($ :header "Keyboard Shortcuts")
         ($ :table.shortcuts
