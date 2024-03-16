20f79c6ffa747b50c6efb88f1fa88d22f2c77cac
Michael Hale <mjhale@users.noreply.github.com>
2024-03-16 17:56:25 -0400

    New backup and restore feature. (#136)

--- src/main/ogres/app/form/help.cljs
+++ src/main/ogres/app/form/help.cljs
@@ -3,7 +3,7 @@
             [ogres.app.hooks :refer [use-dispatch use-query]]
             [ogres.app.shortcut :refer [shortcuts]]
             [ogres.app.provider.release :as release]
-            [uix.core :refer [defui $ use-context]]))
+            [uix.core :refer [defui $ use-context use-ref use-state]]))
 
 (def ^:private confirm-upgrade
   "Upgrading will delete all your local data and restore this application to its original state.")
@@ -11,10 +11,18 @@
 (def ^:private confirm-delete
   "Delete all your local data and restore this application to its original state?")
 
+(def ^:private confirm-backup
+  "Backup your local data and images?")
+
+(def ^:private confirm-restore
+  "Delete all your local data and restore this application using the provided backup?")
+
 (defui form []
-  (let [releases (use-context release/context)
-        dispatch (use-dispatch)
-        result   (use-query [[:local/type :default :conn]] [:db/ident :local])]
+  (let [[file-name set-file-name] (use-state nil)
+        releases  (use-context release/context)
+        dispatch  (use-dispatch)
+        result    (use-query [[:local/type :default :conn]] [:db/ident :local])
+        file-input (use-ref)]
     ($ :.form-help
       (if (= (:local/type result) :host)
         ($ :fieldset.fieldset
@@ -43,6 +51,59 @@
                      (fn []
                        (if-let [_ (js/confirm confirm-delete)]
                          (dispatch :storage/reset)))} "Delete local data")))))))
+      ($ :fieldset.fieldset
+        ($ :legend "Backup and Restore")
+        ($ :div.form-notice
+          ($ :<>
+            ($ :p {:style {:margin-bottom 4}}
+              "Create a backup file that contains all your data and images. You
+               can then use this file to restore your work on another computer
+               or browser.")
+            ($ :button.button.button-neutral
+              {:on-click
+               (fn []
+                 (if-let [_ (js/confirm confirm-backup)]
+                   (dispatch :storage/backup)))} "Create Backup")
+            ($ :br)
+            ($ :p {:style {:margin-bottom 4}}
+              "Select a backup file to restore your data and images. Note that "
+              ($ :strong "restoring from a file will delete all your current data") ".")
+            ($ :form
+              {:class "form-restore"
+               :on-submit
+               (fn [event]
+                 (.preventDefault event)
+                 (let [files (.. file-input -current -files)
+                       file (first files)]
+                   (if-not (nil? file)
+                     (if-let [_ (js/confirm confirm-restore)]
+                       (dispatch :storage/restore file)))))}
+              ($ :div
+                {:style
+                 {:display "flex"
+                  :flex-flow "row rap"
+                  :gap "4px"}}
+                ($ :label
+                  {:for "restore-upload"
+                   :class "button button-neutral"
+                   :style {:box-sizing "border-box"}}
+                  (or file-name "Choose file"))
+                ($ :input
+                  {:type "file"
+                   :name "restore-upload"
+                   :id "restore-upload"
+                   :style {:display "none"}
+                   :accept ".json"
+                   :ref file-input
+                   :on-change
+                   (fn [event]
+                     (let [files (.. event -target -files)
+                           file (first files)]
+                       (set-file-name (.-name file))))})
+                ($ :button.button.button-primary
+                  {:type "submit"
+                   :disabled (if (nil? file-name) true)}
+                  "Restore"))))))
       ($ :div
         ($ :h2 "Keyboard Shortcuts")
         ($ :table.shortcuts
--- src/main/ogres/app/provider/storage.cljs
+++ src/main/ogres/app/provider/storage.cljs
@@ -2,6 +2,9 @@
   (:require [datascript.core :as ds]
             [datascript.transit :as dt]
             [dexie]
+            [dexie-export-import
+             :refer [exportDB importDB peakImportFile]
+             :rename {exportDB export-db importDB import-db peakImportFile peek-import-file}]
             [ogres.app.const :refer [VERSION]]
             [ogres.app.dom :refer [local-type]]
             [ogres.app.provider.events :refer [use-subscribe]]
@@ -102,11 +105,65 @@
     (use-subscribe :tokens/remove-all on-remove-bulk)
     (use-subscribe :scene-images/remove-all on-remove-bulk)))
 
+(defui ^:private backup-handler []
+  (let [store (use-store)]
+    (use-subscribe :storage/backup
+      (use-callback
+       (fn []
+         (.then
+          (export-db store)
+          (fn [blob]
+            (let [blob-url (js/window.URL.createObjectURL blob)
+                  shadow-anchor (js/document.createElement "a")]
+              (set! shadow-anchor -href blob-url)
+              (set! shadow-anchor -download
+                    (str "ogres-"
+                         (first (.split (.toISOString (js/Date.)) "T"))
+                         ".json"))
+              (.click shadow-anchor)
+              (.remove shadow-anchor)))))
+       [store]))))
+
+(defui ^:private restore-handler []
+  (let [store (use-store)]
+    (use-subscribe :storage/restore
+      (use-callback
+       (fn [{[file] :args}]
+         (-> (peek-import-file file)
+             (.then
+              (fn [metadata]
+                (if (= (.-formatName metadata) "dexie")
+                  (.delete store)
+                  (throw (js/Error. "Selected file is not the right format.")))))
+             (.then (fn [] (import-db file)))
+             (.then
+              (fn [^js/object db]
+                (.toArray (.-app db))))
+             (.then
+              (fn [records]
+                (.sort records (fn [^js/object a ^js/object b] (- (.-updated a) (.-updated b))))
+                (let [release (.-release (aget records 0))
+                      params  (js/URLSearchParams. #js {"r" release})]
+                  (.replace (.-location js/window) (str "/?" (.toString params))))))
+             (.catch
+              (fn [error]
+                (js/console.error error)
+                (js/alert
+                 "There was a problem trying to restore from this file. See
+                  developer console for more details.")))))
+       [store]))))
+
 (defui handlers
   "Registers event handlers related to IndexedDB, such as those involved in
    saving and loading the application state." []
   (let [{type :local/type} (use-query [:local/type])]
     (case type
-      :host ($ :<> ($ unmarshaller) ($ marshaller) ($ remove-handler) ($ reset-handler))
+      :host ($ :<>
+              ($ unmarshaller)
+              ($ marshaller)
+              ($ remove-handler)
+              ($ reset-handler)
+              ($ backup-handler)
+              ($ restore-handler))
       :view ($ :<> ($ unmarshaller))
       :conn ($ :<> ($ remove-handler)))))
