ab0192baffda0134c8a398ea30b1ca717006b02a
Sam Ferrell <mail@samcf.me>
2024-04-03 04:14:08 -0600

    Update error boundary styles and messaging.

--- src/main/ogres/app/render.cljs
+++ src/main/ogres/app/render.cljs
@@ -24,16 +24,27 @@
    (fn [[state _] {:keys [children]}]
      (let [store (initialize)]
        (if (:error state)
-         ($ :div {:style {:padding "32px" :max-width 640}}
-           ($ :p "The application crashed! Try refreshing the page. If the problem
-               persists, click the button below to delete your local data. This
-               may resolve your issue.")
-           ($ :br)
-           ($ :button.button
-             {:on-click
-              (fn []
-                (.delete store)
-                (.reload (.-location js/window)))} "Delete your local data"))
+         ($ :.layout-error
+           ($ :.layout-error-content
+             ($ :div {:style {:margin-top 4 :color "hsl(6, 73%, 60%)"}}
+               ($ icon {:name "exclamation-triangle-fill"}))
+             ($ :div
+               ($ :div {:style {:font-size 20 :line-height 1}}
+                 "The application crashed unexpectedly.")
+               ($ :div {:style {:margin-top 16 :margin-bottom 8 :max-width 512}}
+                 "Try refreshing the page; if that doesn't work you may have to
+                  delete your local data. This will reset the application to
+                  its original state, removing your data and images.")
+               ($ :button.button.button-danger
+                 {:on-click
+                  #(.then
+                    (.delete store)
+                    (.reload (.-location js/window)))} "Delete your local data")
+               ($ :nav {:style {:margin-top 32 :color "var(--color-danger-500)"}}
+                 ($ :ul {:style {:display "flex" :gap 16}}
+                   ($ :li ($ :a {:href "/"} "Home"))
+                   ($ :li ($ :a {:href "https://github.com/samcf/ogres/wiki"} "Wiki"))
+                   ($ :li ($ :a {:href "https://github.com/samcf/ogres/discussions"} "Support")))))))
          children)))))
 
 (defui pagination
