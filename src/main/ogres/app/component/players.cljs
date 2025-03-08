e6102914678a88c9341ae752bca706e001059989
Sam Ferrell <mail@samcf.me>
2025-03-08 15:56:05 -0700

    Only display players viewing the current scene.

--- src/main/ogres/app/component/players.cljs
+++ src/main/ogres/app/component/players.cljs
@@ -1,58 +1,47 @@
 (ns ogres.app.component.players
   (:require [ogres.app.component :as component]
             [ogres.app.hooks :as hooks]
-            [ogres.app.util :refer [separate]]
             [uix.core :refer [defui $]]))
 
+(defn ^:private conns-xf [self]
+  (comp (filter (comp #{:conn} :user/type))
+        (filter (comp (complement #{(:user/uuid self)}) :user/uuid))
+        (filter
+         (fn [user]
+           (= (:db/id (:camera/scene (:user/camera user)))
+              (:db/id (:camera/scene (:user/camera self))))))))
+
 (def ^:private query-user
-  [:db/id
-   :user/uuid
+  [:user/uuid
    :user/color
    :user/type
    :user/label
    :user/description
    {:user/image
-    [{:image/thumbnail [:image/hash]}]}])
+    [{:image/thumbnail [:image/hash]}]}
+   {:user/camera
+    [{:camera/scene
+      [:db/id]}]}])
 
 (def ^:private query
-  [{:root/user [:user/uuid :user/type]}
-   {:root/session [:session/room {:session/conns query-user}]}])
+  [{:root/user query-user}
+   {:root/session
+    [:session/room
+     {:session/conns query-user}]}])
 
 (defui players []
   (let [dispatch (hooks/use-dispatch)
-        {{self-uuid :user/uuid self-type :user/type} :root/user {conns :session/conns} :root/session} (hooks/use-query query [:db/ident :root])
-        users (filter (comp #{:conn} :user/type) conns)
-        [[user] users] (separate (comp #{self-uuid} :user/uuid) users)]
+        result   (hooks/use-query query [:db/ident :root])
+        {self :root/user
+         {conns :session/conns} :root/session} result]
     ($ :.players
-      ($ :<>
-        (if (= self-type :conn)
-          (let [{:user/keys [color label description image]} user]
-            ($ :.player-tile {:data-editable true}
-              ($ :.player-tile-color {:data-color color})
-              ($ :button.player-tile-image
-                {:type "button"
-                 :on-click (fn [] (dispatch :user/select-panel :lobby))}
-                ($ :.player-tile-image-frame)
-                (if (not (nil? image))
-                  ($ component/image {:hash (:image/hash (:image/thumbnail image))}
-                    (fn [url]
-                      ($ :.player-tile-image-content
-                        {:style {:background-image (str "url(" url ")")}})))
-                  ($ :.player-tile-image-default
-                    ($ component/icon {:name "dnd"})))
-                ($ :.player-tile-image-edit
-                  ($ component/icon {:name "wrench-adjustable-circle" :size 26})))
-              ($ :.player-tile-content
-                ($ :.player-tile-label
-                  {:data-placeholder "Player character (You)"}
-                  label)
-                ($ :.player-tile-description
-                  {:data-placeholder "Click on portrait to edit"}
-                  description)))))
-        (for [{:user/keys [uuid color label description image]} users]
-          ($ :.player-tile {:key uuid}
+      (if (= (:user/type self) :conn)
+        (let [{:user/keys [color label description image]} self]
+          ($ :.player-tile {:data-editable true}
             ($ :.player-tile-color {:data-color color})
-            ($ :.player-tile-image
+            ($ :button.player-tile-image
+              {:type "button"
+               :on-click (fn [] (dispatch :user/select-panel :lobby))}
               ($ :.player-tile-image-frame)
               (if (not (nil? image))
                 ($ component/image {:hash (:image/hash (:image/thumbnail image))}
@@ -60,7 +49,28 @@
                     ($ :.player-tile-image-content
                       {:style {:background-image (str "url(" url ")")}})))
                 ($ :.player-tile-image-default
-                  ($ component/icon {:name "dnd"}))))
+                  ($ component/icon {:name "dnd"})))
+              ($ :.player-tile-image-edit
+                ($ component/icon {:name "wrench-adjustable-circle" :size 26})))
             ($ :.player-tile-content
-              ($ :.player-tile-label {:data-placeholder "Player character"} label)
-              ($ :.player-tile-description {:data-placeholder ""} description))))))))
+              ($ :.player-tile-label
+                {:data-placeholder "Player character (You)"}
+                label)
+              ($ :.player-tile-description
+                {:data-placeholder "Click on portrait to edit"}
+                description)))))
+      (for [{:user/keys [uuid color label description image]} (sequence (conns-xf self) conns)]
+        ($ :.player-tile {:key uuid}
+          ($ :.player-tile-color {:data-color color})
+          ($ :.player-tile-image
+            ($ :.player-tile-image-frame)
+            (if (not (nil? image))
+              ($ component/image {:hash (:image/hash (:image/thumbnail image))}
+                (fn [url]
+                  ($ :.player-tile-image-content
+                    {:style {:background-image (str "url(" url ")")}})))
+              ($ :.player-tile-image-default
+                ($ component/icon {:name "dnd"}))))
+          ($ :.player-tile-content
+            ($ :.player-tile-label {:data-placeholder "Player character"} label)
+            ($ :.player-tile-description {:data-placeholder ""} description)))))))
