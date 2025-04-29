1e1c82a593ca20de7afca4fef712b4d3995cc216
Sam Ferrell <mail@samcf.me>
2025-04-29 14:03:42 -0600

    Rewrite window bounds listener.

--- src/main/ogres/app/provider/window.cljs
+++ src/main/ogres/app/provider/window.cljs
@@ -1,41 +1,32 @@
 (ns ogres.app.provider.window
   (:require [goog.functions :refer [throttle]]
             [ogres.app.hooks :as hooks]
-            [ogres.app.vec :as vec]
+            [ogres.app.vec :as vec :refer [Vec2 Segment]]
             [uix.core :as uix :refer [defui $]]))
 
-(defui ^:private bounds
-  "Registers event handlers to watch for changes in the scene dimensions in
-   order to put those dimensions in the application state."
-  [{:keys [target]}]
-  (let [dispatch (hooks/use-dispatch)
-        handler  (uix/use-memo
-                  (fn []
-                    (throttle
-                     (fn []
-                       (if-let [element (.. target -document (querySelector ".layout-scene"))]
-                         (let [rect (vec/DOMRect->Segment (.getBoundingClientRect element))]
-                           (dispatch :user/change-bounds rect)))) 100)) [dispatch target])
-        [observer] (uix/use-state (js/ResizeObserver. handler))
-        [scene set-scene] (uix/use-state nil)]
-    (hooks/use-event-listener "resize" handler)
+(defui ^:private register-bounds [_]
+  (let [[retry set-retry] (uix/use-state 0)
+        dispatch (hooks/use-dispatch)
+        listener
+        (uix/use-memo
+         (fn []
+           (throttle
+            (fn [entries]
+              (let [ent (aget entries 0)
+                    rct (.-contentRect ent)
+                    trg (.-target ent)
+                    src (Vec2. (.-offsetLeft trg) (.-offsetTop trg))
+                    dst (vec/shift src (.-width rct) (.-height rct))
+                    bnd (vec/rnd (Segment. src dst))]
+                (dispatch :user/change-bounds bnd))) 256)) [dispatch])
+        [observer] (uix/use-state (js/ResizeObserver. listener))]
     (uix/use-effect
      (fn []
-       ((fn f []
-          (let [element (.querySelector (.-document target) ".layout-scene")]
-            (if element
-              (set-scene element)
-              (.requestAnimationFrame js/window f)))))))
-    (uix/use-effect
-     (fn []
-       (when scene
-         (.observe observer scene)
-         (fn [] (.unobserve observer scene)))) ^:lint/disable [scene])))
+       (if-let [element (js/document.querySelector ".layout-scene")]
+         (do (.observe observer element)
+             (fn [] (.unobserve observer element)))
+         (do (set-retry inc)
+             (fn [])))) [observer retry])))
 
-(defui listeners
-  "Provides a reference to the view window, if any, and registers several
-   event handlers needed for them."
-  []
-  (let [{:keys [user/ready]} (hooks/use-query [:user/ready])]
-    (if ready
-      ($ bounds {:target js/window}))))
+(defui listeners []
+  ($ register-bounds))
